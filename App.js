25197ced2fb1cac230f30c5c788bbb9a3c0bf7d6
kgrundemann <krzgrund@gmail.com>
2023-02-21 21:20:34 +0100

    Hello World

--- App.js
+++ App.js
@@ -1,20 +1,14 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { Text, View } from "react-native";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View
+      style={
+        (styles = { flex: 1, alignItems: "center", justifyContent: "center" })
+      }
+    >
+      <Text>Hello World</Text>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
